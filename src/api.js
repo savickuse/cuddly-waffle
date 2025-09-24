3e224af5940e39bbe490530f31b1a17dd7633751
savickuse <savickuse@users.noreply.github.com>
2025-09-24 18:11:40 +0300

    ci: fix memory leak

--- src/api.js
+++ src/api.js
@@ -77,3 +77,30 @@ class API {
 
 // Update 11
 module.exports = API;
+
+
+// API module for WaffleGrid
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 35
+module.exports = API;
