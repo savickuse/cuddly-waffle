0160f38646fe741bc143d4ecd0b5b85795dd8f53
savickuse <savickuse@users.noreply.github.com>
2026-01-29 07:52:36 +0200

    improve: add comments

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 7
 module.exports = API;
+
+
+// API module for WaffleGrid
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 11
+module.exports = API;
